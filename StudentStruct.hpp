f18492f1b8aeb210837dce19d64f5e19c25bb6de
vadimasKo <kozemiakinas123@gmail.com>
2021-02-26 20:49:27 +0200

    Create StudentSruct.cpp StudentSruct.hpp

---
+++ StudentStruct.hpp
@@ -0,0 +1,16 @@
+#ifndef STUDENT_STRUCT_H
+#define STUDENT_SRUTCT_H
+
+#include <string>
+#include <vector>
+
+struct Student{
+    std::string fName, lName;
+    double finalMed, finalAvrg;
+
+    void setFinalGrade(std::vector<double> &grades, double examRez);
+    bool operator < (Student &obj) const;
+};
+
+
+#endif
